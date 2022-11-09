c3a8e31337e505aa19c3be9f3a6738af7fbcbb5b
ACHIRAFI El Farouk <el-farouk.achirafi@soprasteria.com>
2022-11-09 14:51:51 +0100

    Design de la page de panier

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Cart } from "src/app/models/cart.model";
+import { Cart, CartItem } from "src/app/models/cart.model";
 
 
 @Component({
@@ -9,10 +9,41 @@ import { Cart } from "src/app/models/cart.model";
   ]
 })
 export class CartComponent implements OnInit {
-  cart: Cart = { items: [] };
+  cart: Cart = { items: [
+    {
+      product: 'https://picsum.photos/200/300',
+      name: 'snickers',
+      quantity: 1,
+      price: 150,
+      id: 1,
+    },
+    {
+      product: 'https://picsum.photos/200/300',
+      name: 'snickers',
+      quantity: 1,
+      price: 150,
+      id: 2,
+    }
+  ] };
+  dataSource: CartItem[] = [];
+  displayedColumns: string[] = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource = this.cart.items;
   }
 
+  getTotal(items: CartItem[]): number {
+    return items.
+      map((item: CartItem) => item.price * item.quantity).
+        reduce((prevValue, value) => prevValue + value, 0);
+  }
 }
